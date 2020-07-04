abf4f1fe54daa987b19a708d620833c1adfbac22
WONILLISM <wonillism@gmail.com>
2020-07-04 23:33:08 +0900

    STATUS

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,52 +1,18 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
 
-function Food({name, picture, rating}){
-  return <div>
-    <h2>I like {name}</h2>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name} />
-  </div>
-}
-
-Food.propTypes = {
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired 
-}
-
-const foodILike = [
-  {
-    id:"1",
-    name: "Kimchi", 
-    image:"https://okonomikitchen.com/wp-content/uploads/2020/03/vegan-kimchi-recipe-12-of-14.jpg",
-    rating: 5
-  },
-  {
-    id:"2",
-    name: "Ramen", 
-    image:"https://hips.hearstapps.com/hmg-prod/images/190208-delish-ramen-horizontal-093-1550096715.jpg",
-    rating: 4.9
-  },
-  {
-    id:"3",
-    name: "Potato", 
-    image:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-191907-twice-baked-potatoes-0027-landscape-pf-1564166855.png?crop=0.668xw:1.00xh;0.289xw,0&resize=480:*",
-    rating: 4.8
-  },
-]
-function App() {
-  return (
-  <div>
-    <h1>Hello!</h1> 
-    { foodILike.map(dish => <Food 
-        key={dish.id} 
-        name={dish.name} 
-        picture={dish.image} 
-        rating={dish.rating}
-        />)}
-  </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  componentDidMount() {
+    setTimeout(() => this.setState({ isLoading: false }), 6000);
+  }
+  render() {
+    const { isLoading } = this.state; // ES6에서만 작동
+    // return <div>{this.state.isLoading ? "Loading..." : "We are ready"}</div>; ES6 이하 작동
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
 
 export default App;
