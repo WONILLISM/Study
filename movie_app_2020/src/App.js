41d35d79f116e2d7dc299e4b99541744ffe50800
WONILLISM <wonillism@gmail.com>
2020-07-06 20:44:26 +0900

    Making the movie app 01

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,17 +1,48 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movies";
 
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    // ES6 버전
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    //console.log(movies);
+    // {state 안의 movies : axios의 movies} -> { movies }로 단축할 수 있다.
+    // this.setState({ movies:movies });
+    this.setState({ isLoading: false, movies });
+    // ES6 이전 버전
+    // const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    // console.log(movies.data.data.movies);
   };
   componentDidMount() {
-    setTimeout(() => this.setState({ isLoading: false }), 6000);
+    this.getMovies();
   }
   render() {
-    const { isLoading } = this.state; // ES6에서만 작동
-    // return <div>{this.state.isLoading ? "Loading..." : "We are ready"}</div>; ES6 이하 작동
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state; // ES6에서만 작동
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
 }
 
